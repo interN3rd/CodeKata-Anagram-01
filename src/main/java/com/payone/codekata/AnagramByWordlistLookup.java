c79b2317f2018e76baec9d853aba6bfd450ab5a5
Pascal Groß <pascal.gross@student.fh-kiel.de>
2022-05-05 11:56:48 +0200

    to do: add mechanism to detect that a wordlist is already filtered

--- src/main/java/com/payone/codekata/AnagramByWordlistLookup.java
+++ src/main/java/com/payone/codekata/AnagramByWordlistLookup.java
@@ -10,43 +10,12 @@ public class AnagramByWordlistLookup implements AnagramFinder {
     private static final Pattern PATTERN = Pattern.compile( "^[A-Za-z ]+$" );
     private final List<String> unfilteredWordlist;
     private final List<String> filteredWordlist = new ArrayList<>();
+    private String key;
 
-    public AnagramByWordlistLookup( List<String> unfilteredWordlist ) {
+    public AnagramByWordlistLookup( List<String> unfilteredWordlist, String key ) {
 
         this.unfilteredWordlist = unfilteredWordlist;
-    }
-
-    private List<String> filterWordlist( List<String> unfilteredWordlist ) {
-
-        return unfilteredWordlist.stream()
-                .filter( line -> PATTERN.matcher( line ).matches() )
-                .map( String::toLowerCase )
-                .collect( Collectors.toList() );
-    }
-
-    private String validateAnagramCandidate( final String word ) {
-
-        if( word == null ) {
-
-            throw new IllegalArgumentException( "A value of 'null' was received." );
-        }
-
-        if( word.trim().isEmpty() ) {
-
-            throw new IllegalArgumentException( "No character was received." );
-        }
-
-        if( word.length() > 27 ) {
-
-            throw new IllegalArgumentException( "Received an unexpected value. The longest known word to have anagrams consists of 27 characters." );
-        }
-
-        if( !PATTERN.matcher( word ).find() ) {
-
-            throw new IllegalArgumentException( "A non-alphabetical character was received." );
-        }
-
-        return word.trim().toLowerCase();
+        this.key = key;
     }
 
     /**
@@ -65,6 +34,44 @@ public class AnagramByWordlistLookup implements AnagramFinder {
         if( anagramCandidate.length() == 1 ) {
 
             return new ArrayList<>();
+        }
+
+        if( this.key.equals( "filtered") ) {
+
+            // create every possible letter combination of the word that the user typed in
+            List<String> possibleAnagrams = buildWords( anagramCandidate );
+            if( possibleAnagrams.isEmpty() || possibleAnagrams.size() == 1 ) {
+                return possibleAnagrams;
+            }
+
+            // compare every letter combination with a list of english words. If a match is found, it's an anagram
+            List<String> anagrams = new ArrayList<>();
+
+            // by definition an anagram consists of the same amount of letters like the original word
+            // example: if the user types in "owl" an anagram would be "low"
+            // both words should be in the wordlist
+            // therefore: if the word doesn't even come up in the wordlist, there will be no anagram
+            if( unfilteredWordlist.contains( anagramCandidate ) ) {
+                for ( String anagram : possibleAnagrams ) {
+                    if ( unfilteredWordlist.contains( anagram ) ) {
+                        anagrams.add( anagram );
+                    }
+                }
+            } else {
+                return new ArrayList<>();
+            }
+
+            // "yadayada" is a word that is fully processed but won't produce any anagrams
+            if ( anagrams.isEmpty() ) {
+                return new ArrayList<>();
+            }
+            // "hello" has no anagrams, but at this point anagrams consists of [hello, hello]
+            if ( anagrams.get( 0 ).equals( anagrams.get( 1 ) ) ) {
+                return new ArrayList<>();
+            }
+            // the first entry of anagrams is always the original word which is no anagram
+            return anagrams.subList(1, anagrams.size());
+
         }
 
         // anagrams are found by looking up entries of a wordlist. There has to be a wordlist accessible then
@@ -106,6 +113,39 @@ public class AnagramByWordlistLookup implements AnagramFinder {
         }
         // the first entry of anagrams is always the original word which is no anagram
         return anagrams.subList(1, anagrams.size());
+    }
+
+    private List<String> filterWordlist( List<String> unfilteredWordlist ) {
+
+        return unfilteredWordlist.stream()
+                .filter( line -> PATTERN.matcher( line ).matches() )
+                .map( String::toLowerCase )
+                .collect( Collectors.toList() );
+    }
+
+    private String validateAnagramCandidate( final String word ) {
+
+        if( word == null ) {
+
+            throw new IllegalArgumentException( "A value of 'null' was received." );
+        }
+
+        if( word.trim().isEmpty() ) {
+
+            throw new IllegalArgumentException( "No character was received." );
+        }
+
+        if( word.length() > 27 ) {
+
+            throw new IllegalArgumentException( "Received an unexpected value. The longest known word to have anagrams consists of 27 characters." );
+        }
+
+        if( !PATTERN.matcher( word ).find() ) {
+
+            throw new IllegalArgumentException( "A non-alphabetical character was received." );
+        }
+
+        return word.trim().toLowerCase();
     }
 
     List<String> buildWords( final String word ) {
